214432f33eb32981a0d2f4a6897c54f7064ac27f
Jess Figueiredo <jhessikafeitosa@hotmail.com.br>
2024-05-20 22:25:27 -0300

    add ci papeline

--- cypress.config.js
+++ cypress.config.js
@@ -10,5 +10,12 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
   },
+  scripts: {
+    "cy:open": "cypress open --config-file=cypress.config.js",
+    "cy:open:mobile": "cypress open --config-file=cypress.config.js --config viewportWidth=410 viewportHeight=860",
+    "test": "cypress run --config-file=cypress.config.js",
+    "test:mobile": "cypress run --config-file=cypress.config.js --config viewportWidth=410 viewportHeight=860"
+  }
+
 })
 
